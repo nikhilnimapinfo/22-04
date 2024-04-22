cf827d91c859109bceb417049d0de9ec1dce2dbc
nikhilnimapinfo <166018854+nikhilnimapinfo@users.noreply.github.com>
2024-04-22 18:17:32 +0530

    Add files via upload

---
+++ Generi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Generic
+{
+    public class Generi<String>
+    {
+        public String s;
+        public Generi(String i)
+        {
+            s = i;
+        }
+
+        public String MyMethod()
+        {
+            return s;
+        }
+        public void m1()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        
+        public int m2<String>(String h)
+        {
+            Console.WriteLine(h);
+            return 199999;
+        }
+    }
+
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Lambda_Expression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          Func<int,int>Squre= i=>  i* i;
+            Console.WriteLine(Squre(5));
+            string[] arr = new string[] { "A", "B" ,"C"};
+
+           
+            for (int a=0;a < arr.Length;a++) {
+                Func<string, string> NAME = d => d + d+d;
+                Console.WriteLine(NAME(arr[a]));
+            }
+
+            foreach (var r in arr)
+            {
+                Func<string, string> NAME = d => d + d + d;
+                Console.WriteLine(NAME(r));
+            }
+           
+        }
+       
+    }
+}
